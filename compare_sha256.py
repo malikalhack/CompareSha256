16d0d16d8bb25e18ac0d5ef5cdd3ba25238d97b7
Anton Chernov <malikalhack@gmail.com>
2026-04-24 00:21:50 +0300

    Added version check

--- compare_sha256.py
+++ compare_sha256.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+SHA256 Checksum Calculator and Verifier
+
+Author: Anton Chernov
+Date: 2026-02-09
+License: MIT
+"""
+
+import sys
+
+
+def validate_py_version() -> bool:
+    bool_result = True
+    major_version = sys.version_info.major
+    minor_version = sys.version_info.minor
+    if not (major_version == 3 and minor_version >= 8):
+        print("Python 3.8 or higher is required.")
+        print(f"You are using Python {major_version}.{minor_version}")
+        bool_result = False
+    return bool_result
 
 
 if __name__ == "__main__":
-    pass
+    # Check Python version
+    if not validate_py_version():
+        sys.exit(1)
+    sys.exit(0)
+
